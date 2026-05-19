700e7881c4e64ef72189222300d96af392b8e8be
Pratheeksha27032007 <pratikshand3@gmail.com>
2026-05-20 00:10:53 +0530

    first calci

---
+++ script.js
@@ -0,0 +1,63 @@
+display=document.querySelector(".display")
+clear=document.querySelector(".clear")
+nums=document.querySelectorAll(".nums")
+ops=document.querySelectorAll(".ops")
+solve=document.querySelector(".solve")
+let equation="",solved=false;
+let disp=(a)=>{
+    if(solved){
+        equation=""
+        display.innerText=""
+        solved=false
+    }
+    //edge cases
+    if(a=="x"){equation+="*";display.innerText+=a}
+    else if(a=="x2"){equation+="**2";display.innerText+="^2"}
+    else if(a=="sqrt"){equation+="**0.5";display.innerText+="sqrt()"}
+    else{ equation+=a;
+    display.innerText+=a;}
+}
+nums.forEach((num)=>{
+    num.addEventListener("click",()=>{
+        disp(num.innerText);
+        console.log(num.innerText)
+    })
+})
+ops.forEach((op)=>{
+    op.addEventListener("click",()=>{
+        console.log(op.innerText)
+        disp(op.innerText);
+    })
+})
+document.addEventListener("keydown",(e)=>{
+    let allowed="0123456789+-*/%."
+    if(allowed.includes(e.key))disp(e.key); 
+    console.log(e.key)  
+    if(e.key=="Backspace"){
+    equation=equation.slice(0,-1)
+    display.innerText=display.innerText.slice(0,-1)
+    }
+    if(e.key=="Enter"){
+    solve.click();
+    }
+ })
+
+clear.addEventListener("click",()=>{
+    equation="";
+    display.innerText=""
+})
+solve.addEventListener("click",()=>{
+    try{
+    let ans=eval(equation)
+    equation=ans.toString()
+    console.log(ans)
+    display.innerText=ans;
+    solved=true;
+    }
+    catch{
+        display.innerText="Error"
+        equation=""
+        solved=true;
+    }
+})
+
